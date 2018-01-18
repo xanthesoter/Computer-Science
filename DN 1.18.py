4f9cbbee6456433f21ddd95d21a430ada1d55a76
xanthesoter <34690979+xanthesoter@users.noreply.github.com>
2018-01-18 13:30:48 -0500

    Add files via upload

---
+++ DN 1.18.py
@@ -0,0 +1,41 @@
+""" #1 """
+#variable
+x = 3
+#string
+print('hello')
+#integer
+print(4)
+#float
+print(4.3)
+
+""" #2 """
+# (4<5) and (5>6) this will evaluate to True
+
+""" #3 """
+#I know there is an algorithm to it but because I don't know it I would honestly just add 0 - 100
+
+""" #4 """
+total = 0
+for number in range(100):
+    total = total + number
+    print(total)
+
+""" #5 """
+# 'hello' == 'Hello' would be false
+# True != False would be true
+# 42 == '42' would be false
+""" myAge=29 
+myAge>=15 while this would be true
+myAge==15 this would be false """
+
+""" #6 """
+for number in range(30):
+    if number%2 == 0:
+        print(number, 'is an even number')
+
+""" #7 """
+for number in range(15, 30):
+    if number%2 == 0:
+        print(number, "is an even number")
+
+
