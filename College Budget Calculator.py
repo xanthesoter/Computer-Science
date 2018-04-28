44499881a8c5d87c8d587d205009234670d2cb9e
xanthesoter <34690979+xanthesoter@users.noreply.github.com>
2018-04-28 18:32:48 -0400

    Add files via upload

--- College Budget Calculator.py
+++ College Budget Calculator.py
@@ -1,39 +1,56 @@
 # College Budget Calculator for
-# books, spending money, travel, loans, scholarships, grants, fees
 
-import random
 import time
 
 print(time.asctime())
 
-def authenticate():
-     total_triesUser=0
-     total_triesPass=0
-     while total_triesUser<4 and total_triesPass<4:
-          theUsername=str(input("Enter your username, the press Return/Enter: "))
-          if theUsername== "Codemaster":
-               total_triesUser+=4
-               print("Username successful")
-               while total_triesPass<4:
-                     thePassword=str(input("Enter the password, then press Return/Enter: "))
-                     if thePassword== "201010":
-                          total_triesPass+=4
-                          print("Password successful")
-                     else:
-                          print("Unable to log in. ")
-                          total_triesPass+=1
-                          if total_triesPass>3:
-                               break                         
-          else:
-               print("Username Incorrect.")
-               total_triesUser+=1
-               if total_triesUser>3:
-                    break
+def username():
+    username = input("Enter what you would like your username to be: ")
+    print("Username created successfully")
+    return username
+def password():
+    password = input("Enter what you would like your password to be: ")
+    print("Password created successfully")
+    return password
 
- True:
+username = username()
+password = password()
+
+def login():
+    total_triesUser=0
+    total_triesPass=0
+    print("Your account was created successfully now you must login.")
+    while total_triesUser<4 and total_triesPass<4:      
+        theUsername = str(input("Enter your username, the press Return/Enter: "))
+        if theUsername == username:
+            total_triesUser+=4
+            print("Username successful")
+            while total_triesPass<4:
+                thePassword=str(input("Enter the password, then press Return/Enter: "))
+                if thePassword == password:
+                    total_triesPass+=4
+                    print("Password successful")
+                else:
+                    print("Unable to log in. ")
+                    total_triesPass+=1
+                    if total_triesPass>3:
+                        print("You have been locked out from attempting to login too many times.")
+                        break
+        else:
+            print("Username Incorrect.")
+            total_triesUser+=1
+            if total_triesUser>3:
+                print("You have been locked out from attempting to loging too many times.")
+                break
+
+login()
+
+while True:
+    print('''Welcome to the college calculator!
+Follow the questions and in the end a reciept based on your anwsers will be printed.''')
     def tuition():
-        userInput = input("How much is tuition?: ")
-        return(userInput)
+        userInput = int(input("How much is tuition?: "))
+        return userInput
     def room():
         userInput = input(""" Dorm room types:
                                     A. Single
@@ -44,17 +61,21 @@ def authenticate():
                                     F. Apartment
                                     Which type of room do you want to dorm in?: """)
         if userInput == "A" or userInput == "a":
-            return(12,194)
+            return 12194
         elif userInput == "B" or userInput == "b":
-            return(11,356)
+            return 11356
         elif userInput == "C" or userInput == "c":
-            return(10,793)
+            return 10793
         elif userInput == "D" or userInput == "d":
-            return(13,304)
+            return 13304
         elif userInput == "E" or userInput == "e":
-            return(11,627)
+            return 11627
         elif userInput == "F" or userInput == "f":
-            return(13,239)                                                
+            return 13239
+        else:
+            print("That isn't an option.")
+            return room()
+
     def meal_plan():
         userInput = input("""Meal Plans:
                                         A. Regular 5
@@ -64,15 +85,19 @@ def authenticate():
                                         E. Unlimited
                                         What type of meal plan do you want?: """)
         if userInput == "A" or userInput == "a":
-            return(245)
+            return 245
         elif userInput == "B" or userInput == "b":
-            return(345)
+            return 345
         elif userInput == "C" or userInput == "c":
-            return(350)
+            return 350
         elif userInput == "D" or userInput == "d":
-            return(450)
+            return 450
         elif userInput == "E" or userInput == "e":
-            return(550)
+            return 550
+        else:
+            print("That isn't an option.")
+            return meal_plan()
+        
     def books():
         userInput = input("Do you plan on purchasing hardcopies or digital copies of your books? Enter A for hardcopy or B for digital: ")
         if userInput == "A" or userInput == "a":
@@ -80,67 +105,79 @@ def authenticate():
             if userInput == "A" or userInput == "a":
                 userInput = input("Do you want a new or used copy? Enter A for new or B for used: ")
                 if userInput == "A" or userInput == "a":
-                    return(2000)
+                    return 2000
                 elif userInput == "B" or userInput == "b":
-                    return(1000)
+                    return 1000
             elif userInput == "B" or userInput == "b":
-                 userInput = input("Do you want a new or used copy? Enter A for new or B for used: ")
+                userInput = input("Do you want a new or used copy? Enter A for new or B for used: ")
                 if userInput == "A" or userInput == "a":
-                    return(3150)
+                    return 3150
                 elif userInput == "B" or userInput == "b":
-                    return(1900)
+                    return 1900
         elif userInput == "B" or userInput == "b":
-            return(950)
+            return 950
+        else:
+            print("That isn't a value.")
+            return books()
+
     def spending_money():
-        userInput = input("How much do you plan to bring to spend per month?: ")
+        userInput = int(input("How much do you plan to bring to spend per month?: "))
         cost = userInput * 9
-        return(cost)
+        return cost
+ 
     def travel():
-        userInput = input("How far away from campus do you live? Enter distance in miles: ")
+        userInput = int(input("How far away from campus do you live? Enter distance in miles: "))
         if userInput <= 50:
-            return(100)
+            return 100
         elif userInput <= 100:
-            return(250)
+            return 250
         elif userInput <= 500:
-            return(700)
+            return 700
         elif userInput <= 1000:
-            return(2000)
+            return 2000
         elif userInput > 1000:
-            return(3000)
+            return 3000
+        else:
+            print("That isn't an option.")
+            return travel()
+
     def deductions():
-        scholarship = float(input("Did you get any scholarships, grants, awards, etc.? Enter the total here: "))
-        loans = float(input("How much are you taking out in loans?: "))
+        scholarship = int(input("Did you get any scholarships, grants, awards, etc.? Enter the total here: "))
+        loans = int(input("How much are you taking out in loans?: "))
         totaldeductions = scholarship + loans
-        return(totaldeductions)
+        return totaldeductions
+
     def fees():
-        return(800)
+        return 800
 
-"""
-    
-    discount = input("Do you have a discount code? If so, enter here: ")
-    if discount == "15OFF":
-        new_total = total - (total * 0.15)
-    else:
-        new_total = total + 0
-    
-    tax = 0.07
-    total_tax = int(new_total * tax)
-    final_total = new_total + total_tax
+    def reciept(tuition_and_fees, room_and_board, books, spending_money, travel, total_deductions, cost):
+        print("")
+        print("RECIEPT:")
+        print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("| Tuition and Fees: $",tuition_and_fees)
+        print("| Room and Board: $",room_and_board)
+        print("| Books: $",books)
+        print("| Spending Money: $",spending_money)
+        print("| Travel Expenses: $",travel)
+        print("| Total Aid: $",total_deductions)
+        print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Your expected fall 2018/2019 cost: $",cost)
 
-    print("")
-    print("RECIEPT:")
-    print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    print("| Total: $",total)
-    print("| Total with any discounts: $",new_total)
-    print("| Tax: $",total_tax,)
-    print("| Your total trip cost is",final_total,"dollars.")
-    print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    tuition_and_fees = tuition() + fees()
+    room_and_board = room() + meal_plan()
+    books = books()
+    spending_money = spending_money()
+    travel = travel()
+    total_deductions = deductions()
+    cost = tuition_and_fees + room_and_board + books + spending_money + travel - total_deductions
 
-    answer = input("Do you want to calculate the cost of another trip?: ")
-    if answer == "Yes" or answer == "yes" or answer == "Y" or answer == "y":
+    reciept(tuition_and_fees, room_and_board, books, spending_money, travel, total_deductions, cost)
+
+
+
+    answer = input("Do you want to calculate the cost again?: ")
+    if answer == "Yes" or answer == "yes" or  answer == "Y" or answer == "y":
         continue
     else:
-        print("Have a good trip!")
+        print("Have fun being in debt!")
         break
-
-"""
